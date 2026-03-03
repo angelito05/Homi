75fe23d4f7fbaf7b71e6bbf3f993fef31550e280
Angelito <tachiangelito@gmail.com>
2026-03-03 04:39:34 -0600

    preparacion para subir a render

--- config.py
+++ config.py
@@ -3,13 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SESSION_COOKIE_HTTPONLY = True  # Evita que JavaScript acceda a la cookie (protección XSS)
-SESSION_COOKIE_SECURE = False    # Solo envía cookies sobre HTTPS (requiere certificado SSL)
-SESSION_COOKIE_SAMESITE = 'Lax' # Protección adicional contra CSRF
+SESSION_COOKIE_HTTPONLY = True  
+SESSION_COOKIE_SECURE = True    
+SESSION_COOKIE_SAMESITE = 'Lax' 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretke291762y")
-    MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://Angelito:Angelito05@cluster0.ifetmab.mongodb.net/")
-    CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME", "dgijgaoyp")
-    CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY", "367923394437628")
-    CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET", "Q3Lj95oGK2FXaKUu_dfo-j5cNLI")
+
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'tu-clave-secreta-local-muy-dificil')
+    
+    MONGODB_URI = os.environ.get('MONGODB_URI')
+    
+    CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
+    CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
+    CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
