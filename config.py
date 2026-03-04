2e61ee971863e23bcf5288dea436b8d11f429f9f
Angelito <tachiangelito@gmail.com>
2026-03-04 11:46:28 -0600

    CSS

--- config.py
+++ config.py
@@ -3,16 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SESSION_COOKIE_HTTPONLY = True  
-SESSION_COOKIE_SECURE = True    
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True  
 SESSION_COOKIE_SAMESITE = 'Lax' 
 
 class Config:
-
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    
-    MONGODB_URI = os.environ.get('MONGODB_URI')
-    
-    CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
-    CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
-    CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    MONGODB_URI = os.getenv("MONGODB_URI")
+    CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
